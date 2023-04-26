fe98e87d020beadbd47d7bf8b54d2aae3812da9a
ozgebektas <ozgebektas53@gmail.com>
2023-04-26 15:03:58 +0300

    commit1

--- src/functions.js
+++ src/functions.js
@@ -1,103 +1,115 @@
-function addToCart(quantity,productName="Elma") {
-    console.log("Sepete eklendi : ürün : " 
-    + productName + " adet : " + quantity)
+//FUNCTIONS
+/*function addToCart(productName="elma",quantity) {//elma burada default değer
+    console.log("sepete eklendi!: ürün :"+productName+" adet:"+quantity);
 }
-
 //addToCart()
-addToCart(10)
-//addToCart(15)
+addToCart(10);//böyle bir durumda 10 ilk parametreye eklenir .bunun yerine default belirttiğin productName i en sona yaz
+//addToCart(15);*/
 
-let sayHello = ()=>{
-    console.log("Hello World!")
+/*function addToCart(quantity,productName="elma"){
+    console.log("sepete eklendi!: ürün :"+productName+" adet:"+quantity);
+
 }
+//addToCart(10);
 
+let sayHello=()=>{
+    console.log("hello world")
+}
 sayHello()
 
-let sayHello2 = function () {
-    console.log("Hello World 2")
+
+let sayHello2=function(){
+    console.log("hello world 2");
 }
+sayHello2()*/
 
-sayHello2();
-
-function addToCart2(productName, quantity, unitPrice) {
+/*function addToCart2(productName,quantity,unitPrice) {
     
 }
+addToCart2("elma",2,20)
+addToCart2("armut",3,15)
+addToCart2("elma",6,26)*/
 
-addToCart2("Elma",5,10)
-addToCart2("Armut", 2, 20)
-addToCart2("Limon",3,15)
 
-let product1 = {productName:"Elma", unitPrice:10, quantity:5}
+/*let product1 = {
+    productName: "elma",
+    unitPrice: 10,
+    quantity: 5
+}
 
 function addToCart3(product) {
-    console.log("Ürün : "+product.productName)
-    console.log("Adet : "+product.quantity)
-    console.log("Fiyat : "+product.unitPrice)
+    console.log("ürün:" + product.productName + " adedi:" + product.quantity + " fiyat:" + product.unitPrice)
+}*/
+//obje notasyonu
+
+/*addToCart3(product1)
+let product2 = {
+    productName: "elma",
+    unitPrice: 10,
+    quantity: 5
 }
+let product3 = {
+    productName: "elma",
+    unitPrice: 10,
+    quantity: 5
+}
+product2=product3
+product3.productName="mandalina";
+console.log(product2.productName)//ekrana mandalin yazar. obje:referans tipdir*/
+
+/*let sayi1=50
+let sayi2=100
+sayi1=sayi2;
+sayi2=150
+console.log(sayi1)//number=değer tip olfukları için sayi1 100 değerini alır ve sayi2 ile iletişimini koparır*/
 
 
-addToCart3(product1)
-
-let product2 = {productName:"Elma", unitPrice:10, quantity:5}
-let product3 = {productName:"Elma", unitPrice:10, quantity:5}
-product2 = product3
-product2.productName = "KARPUZ"
-console.log(product3.productName)
-
-let sayi1 = 10
-let sayi2 = 20
-sayi1 = sayi2
-sayi2 = 100
-console.log(sayi1)
-
-function addToCart4(products) {
+function addToCart4(products){
     console.log(products)
+
 }
-
-let products = [
-    {productName:"Elma", unitPrice:10, quantity:5},
-    {productName:"Armut", unitPrice:10, quantity:5},
-    {productName:"Karpuz", unitPrice:10, quantity:5}
+//array içinde objelerde tutulabilir
+let products =[
+    {productName:"Elma",quantity:5,unitPrice:20},
+    {productName:"Armut",quantity:5,unitPrice:20},
+    {productName:"Karpuz",quantity:5,unitPrice:20}
 ]
+//addToCart4(products)
 
-addToCart4(products)
-
-function add(bisey,...numbers) { //rest
-    let total = 0;
+/*function add(bisey,...numbers){//...numbers array haline gelir=REST OPERATÖRÜ.burada elinde bir arary olmadığına dikkat et
+    let total=0;
     for (let i = 0; i < numbers.length; i++) {
-        total = total + numbers[i]
+      total=total+numbers[i]
     }
-    console.log(total)
-    console.log(bisey)
+   // console.log(total)
+   // console.log(bisey)
+   
 }
 
 add(20,30)
-//add(20,30, 40)
-//add(20,30, 40, 50)
+add(20,30,40,510,50)*/
 
-let numbers = [30,10,500,600,120]
-//console.log(...numbers)
-console.log(Math.max(...numbers))
 
-let [icAnadolu,marmara,karadeniz,[icAnadoluSehirleri]] = [
-    {name:"İç Anadolu", population:"20M"},
-    {name:"Marmara", population:"30M"},
-    {name:"Karadeniz", population:"10M"},
+
+/*
+let numbers=[10,20,30,40,50,60]
+console.log(Math.max(...numbers))//sana gelen numbers arrayini ayırmaya yarar*/
+
+let [icAnadolu,marmara,karadeniz,[icAnadoluSehirleri]]=[
+    {name:"iç anadalu",population:"20M"},
+    {name:"marmara",population:"30M"},
+    {name:"karadeniz",population:"10M"},
     [
         ["Ankara","Konya"],
-        ["İstanbul","Bursa"],
-        ["Sinop","Trabzon"],
+        ["istanbul","bursa"],
+        ["sinop","Trabzon"],
     ]
 ]
-
-// console.log(icAnadolu.name)
-// console.log(icAnadolu.population)
+console.log(icAnadolu)
+console.log(marmara)
+console.log(karadeniz)
 console.log(icAnadoluSehirleri)
-
-let newProductName, newUnitPrice, newQuantity
-({productName:newProductName, unitPrice:newUnitPrice, quantity:newQuantity } 
-= {productName:"Elma", unitPrice:10, quantity:5})
-
-console.log(newProductName)
-console.log(newUnitPrice)
-console.log(newQuantity)
+let newproductName,newquantity,newunitPrice
+({ productName:newproductName,quantity:newquantity,unitPrice:newunitPrice} 
+={productName:"Elma",quantity:5,unitPrice:20})
+console.log(newproductName)
